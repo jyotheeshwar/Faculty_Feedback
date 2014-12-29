27568201b34aff7eaf8f58ea081c341a9458a2dc
Jyotheeswar Arvind Manickavasagar <jyotheeshwar@gmail.com>
2014-12-29 15:56:36 -0500

    Cleaned up static reference
    Added Angular variables instead of static reference

--- public/app.js
+++ public/app.js
@@ -3,5 +3,7 @@
     '$scope',
     function ($scope) {
         $scope.test = 'Faculty rating system | ' + (new Date()).getFullYear();
+        $scope.User_name='Insanity Puppy';
+        $scope.university_name='Le Universität Uni';
 }]);
 
