f8e3c183c7b18a182178fc646fcde4ade37b2bbe
Jyotheeswar Arvind Manickavasagar <jyotheeshwar@gmail.com>
2014-12-30 00:48:54 -0500

    Respond with JSON objects from server
    Node responds with JSON objects on GET request from Angular

--- public/app.js
+++ public/app.js
@@ -1,9 +1,16 @@
-﻿angular.module('TestAngular', [])
-.controller('MainCtrl', [
-    '$scope',
-    function ($scope) {
+﻿(function(){
+var app=angular.module('TestAngular', []);
+
+app.controller('MainCtrl', [
+    '$scope','$http',
+    function ($scope,$http) {
         $scope.test = 'Faculty rating system | ' + (new Date()).getFullYear();
         $scope.User_name='Insanity Puppy';
         $scope.university_name='Le Universität Uni';
+        $scope.ratings=[];
+       $http.get('/data').success(function(data){
+        	$scope.ratings=data;
+        });
 }]);
+})();
 
--- server.js
+++ server.js
@@ -4,6 +4,28 @@ var port = process.env.port || 1337;
 var app = express();
 var logger = require('./modules/logger.js');
 
+var ratings = {
+  1:{
+    name: 'Rooney',
+    rating: 'Stunner'
+  },
+  2:{
+    name: 'Macheda',
+    rating: 'Machedaaaaaaaaaaaaaaa'
+  },
+  3:{
+    name: 'Mata',
+    rating: 'In the end, it doesnt even Mataaaaaaaaaaa'
+  },
+  4:{
+  	name: 'Ronaldo',
+  	rating: 'Stunner! ABSOLUTE STUNNER'
+  },
+  5:{
+  	name: 'di Maria',
+  	rating: 'Oh Maria, oh maria, oh maria oh maria'
+  }
+};
 app.use(logger);    // Use a log file? (providers generally log everything)
 
 app.use(express.static('public'));
@@ -15,6 +37,9 @@ app.use(function (req, res, next) {
 
 app.get('/', function (req, res) {
     res.redirect('/public/index.html');
+});
+app.get('/data',function(req,res){
+	res.json(ratings);
 });
 
 app.listen(port);
