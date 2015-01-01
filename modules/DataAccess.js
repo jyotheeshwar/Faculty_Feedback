92a8e5cf447486f2069e55b2f3e323a286990010
Ritikesh <ritikeshsisodiya@gmail.com>
2015-01-01 18:13:29 +0530

    Basic signup and signin working model

---
+++ modules/DataAccess.js
@@ -0,0 +1,48 @@
+﻿var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+    console.log("Connection Opened");
+});
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    username: String,
+    password: String
+});
+
+var User = mongoose.model('User', userSchema);
+    
+var Signup = function (username, password, res) {
+    
+    var user = new User({
+        username: username, password: password
+    });
+    user.save(function () {
+        res.send("user created successfully");
+    });
+}
+
+var Signin = function (username, password, res) {
+    User.findOne({ username: username, password: password }, {username: 1}, function (err, data) {
+        if (data)
+            res.send("Succesfully logged in as: " + data.username);
+        else
+            res.send("Invalid credentials. Please try again.");
+    });
+}
+
+var Users = function (res) {
+    User.find({}, {}, function (err, data) { 
+        //if (data.length > 0)
+        res.json(data);
+    });
+}
+
+module.exports.Users = Users;
+module.exports.Signup = Signup;
+module.exports.Signin = Signin;
--- server.js
+++ server.js
@@ -1,45 +1,35 @@
 ﻿var express = require('express');
+var bodyParser = require('body-parser');
+
+var DataAccess = require('./modules/DataAccess.js');
+var logger = require('./modules/logger.js');
+
 
 var port = process.env.port || 1337;
 var app = express();
-var logger = require('./modules/logger.js');
 
-var ratings = {
-  1:{
-    name: 'Rooney',
-    rating: 'Stunner'
-  },
-  2:{
-    name: 'Macheda',
-    rating: 'Machedaaaaaaaaaaaaaaa'
-  },
-  3:{
-    name: 'Mata',
-    rating: 'In the end, it doesnt even Mataaaaaaaaaaa'
-  },
-  4:{
-  	name: 'Ronaldo',
-  	rating: 'Stunner! ABSOLUTE STUNNER'
-  },
-  5:{
-  	name: 'di Maria',
-  	rating: 'Oh Maria, oh maria, oh maria oh maria'
-  }
-};
-app.use(logger);    // Use a log file? (providers generally log everything)
-
+app.use(logger);
 app.use(express.static('public'));
 
-app.use(function (req, res, next) {
-    console.log("Date.now().toString()");
-    next();
-});
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.redirect('/public/index.html');
 });
-app.get('/data',function(req,res){
-	res.json(ratings);
+
+app.get('/users', function (req, res) { 
+    DataAccess.Users(res);
+});
+
+app.post('/signin', function (req, res){
+    DataAccess.Signin(req.body.username, req.body.password, res);
+});
+
+app.post('/signup', function (req, res) {
+    DataAccess.Signup(req.body.username, req.body.password, res);
 });
 
 app.listen(port);
